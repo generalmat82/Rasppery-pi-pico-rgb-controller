e1183c427ea3bd7df87f357d50468db87e4255d2
generalmat82 <generalmat 81@gmail.com>
2023-08-22 13:07:26 -0400

    made good looking buttons

--- web server/basic-web-app-tutorial/frontend/script.js
+++ web server/basic-web-app-tutorial/frontend/script.js
@@ -234,3 +234,20 @@ document.getElementById("my-own-language").addEventListener("submit", function(e
 function submit_code_callback(){
     console.log("warioj")
 }
+
+
+const handleOnMouseMove = e => {
+    const {currentTarget: target} = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+// document.body.onmousemove = e => handleOnMouseMove(e);
+
+for(const card of document.querySelectorAll("button")){
+    card.onmousemove = e => handleOnMouseMove(e);
+}
